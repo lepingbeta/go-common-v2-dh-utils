e61ac77d8594d08964908f21c4be6046ba0bcc0d
Symphony <zhangleping@cezhiqiu.com>
2024-05-27 11:21:25 +0800

    新增Struct2BsonM

--- utils.go
+++ utils.go
@@ -2,8 +2,8 @@
  * @Author       : Symphony zhangleping@cezhiqiu.com
  * @Date         : 2024-05-08 08:09:45
  * @LastEditors  : Symphony zhangleping@cezhiqiu.com
- * @LastEditTime : 2024-05-15 12:36:08
- * @FilePath     : /v2/go-common-v2-dh-utils/utils.go
+ * @LastEditTime : 2024-05-22 20:35:40
+ * @FilePath     : /hecos-v2-api/data/mycode/dahe/go-common/v2/go-common-v2-dh-utils/utils.go
  * @Description  :
  *
  * Copyright (c) 2024 by 大合前研, All Rights Reserved.
@@ -33,6 +33,23 @@ func Struct2BsonD(doc interface{}) (bson.D, error) {
 
 	// 将BSON字节序列解码为bson.D
 	var bsonDoc bson.D
+	err = bson.Unmarshal(data, &bsonDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	return bsonDoc, nil
+}
+
+func Struct2BsonM(doc interface{}) (bson.M, error) {
+	// 将结构体编码为BSON字节序列
+	data, err := bson.Marshal(doc)
+	if err != nil {
+		return nil, err
+	}
+
+	// 将BSON字节序列解码为bson.D
+	var bsonDoc bson.M
 	err = bson.Unmarshal(data, &bsonDoc)
 	if err != nil {
 		return nil, err
